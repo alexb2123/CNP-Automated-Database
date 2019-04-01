dac1ddaf083ef8b65c38e59124aaf506256c3c27
alexb2123 <alexb2123@gmail.com>
2019-03-31 20:04:12 -0400

    Upload to shared drive
    Uses OAuth and REST API to push csv to shared drive

---
+++ quickstart.py
@@ -0,0 +1,69 @@
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from apiclient.http import MediaFileUpload
+
+
+
+
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/drive']
+
+def main():
+    """Shows basic usage of the Drive v3 API.
+    Prints the names and ids of the first 10 files the user has access to.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                '/Users/alexandrubordei/PycharmProjects/NYCJobs/client_secrets.json', SCOPES)
+            creds = flow.run_local_server()
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('drive', 'v3', credentials=creds)
+
+    # Call the Drive v3 API
+    results = service.files().list(
+        pageSize=10, fields="nextPageToken, files(id, name)").execute()
+    items = results.get('files', [])
+
+    if not items:
+        print('No files found.')
+    else:
+        print('Files:')
+        for item in items:
+            print(u'{0} ({1})'.format(item['name'], item['id']))
+
+    # Will upload file
+    file_metadata = {
+        'name': 'My Report',
+        'mimeType': 'application/vnd.google-apps.spreadsheet'
+    }
+    media = MediaFileUpload('/Users/alexandrubordei/PycharmProjects/NYCJobs/nycjobs_feb_data.csv',
+                            mimetype='text/csv',
+                            resumable=True)
+    file = service.files().create(body=file_metadata,
+                                media_body=media,
+                                fields='id').execute()
+    print(f'File ID: {file.get("id")}')
+
+
+
+if __name__ == '__main__':
+    main()
